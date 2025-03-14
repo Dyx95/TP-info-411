1805ef8c3b432006b457c19974f8464049f5e234
Dyx95 <aramaharivo73@gmail.com>
2025-03-14 14:27:17 +0100

    modif icon

--- connect.php
+++ connect.php
@@ -4,15 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Solo Leveling (DESTRUCTION)</title>
-    <link rel="stylesheet" href="css/index.css">
+    <title>Bienvenue JOUEUR</title>
     <link rel="icon" href="img/homer singe.jpg" type="image/x-icon">
 </head>
 <body>
-    <div>
-        <img id="homer" src="img/homer singe.jpg" alt="homer">
-    </div>
-    
-    <script src="js/index.js" defer></script>
+    <div id="Centre"></div>
+    <script src="js/script.js" defer></script>
 </body>
 </html>
