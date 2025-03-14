9002adb22a72de639337c00c16768ea41c998c46
Dyx95 <aramaharivo73@gmail.com>
2025-03-14 14:35:30 +0100

    ajout form

--- connect.php
+++ connect.php
@@ -6,9 +6,25 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bienvenue JOUEUR</title>
     <link rel="icon" href="img/homer singe.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="css/connect.css">
 </head>
 <body>
-    <div id="Centre"></div>
+    <div id="form">
+        <form class="login-form">
+            <h1>Connexion</h1>
+            <input type="text" placeholder="Nom d'utilisateur" required>
+            <input type="password" placeholder="Mot de passe" required>
+            <button type="submit">Se connecter</button>
+        </form>
+        <form class="signup-form">
+            <h1>Inscription</h1>
+            <input type="text" placeholder="Nom complet" required>
+            <input type="email" placeholder="Adresse email" required>
+            <input type="password" placeholder="Mot de passe" required>
+            <input type="password" placeholder="Confirmez le mot de passe" required>
+            <button type="submit">S'inscrire</button>
+        </form>
+    </div>
     <script src="js/script.js" defer></script>
 </body>
 </html>
