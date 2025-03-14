7249219bfde007db9ee7c251dde230f9af620d96
Dyx95 <aramaharivo73@gmail.com>
2025-03-14 14:15:15 +0100

    ajout et envoie serv

--- Index.php
+++ Index.php
@@ -6,8 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Solo Leveling (DESTRUCTION)</title>
     <link rel="stylesheet" href="css/index.css">
+    <link rel="icon" href="homer singe.jpg" type="image/x-icon">
 </head>
 <body>
+    <img src="homer singe.jpg" alt="homer">
     <script src="js/index.js" defer></script>
 </body>
 </html>
